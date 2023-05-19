cfa486b6ca28f90356ee1eea4aa857485730715a
RayHE <rayhe88@gmail.com>
2023-05-19 13:44:18 -0500

    C++ case for sycl function for adding vectors

---
+++ C_sycl_case/addVec.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <new>
+#include "func.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    double *a, *b, *c;
+    int i, nelements;
+
+    if (argc == 2)
+    {
+        nelements = atoi(argv[1]);
+    }
+    else
+    {
+        nelements = 1000;
+    }
+
+    printf(" Sum of two vectors:\n");
+    printf("    c[i] = a[i] + b[i]\n");
+    printf("    Test for %6d elements\n\n", nelements);
+
+    // Allocate memory
+
+    a = new double[nelements];
+    b = new double[nelements];
+    c = new double[nelements];
+
+    assert(a);
+    assert(b);
+    assert(c);
+
+    // Fill the vector
+    for (i = 0; i < nelements; i++)
+    {
+        a[i] = 1.;
+        b[i] = 2.;
+        c[i] = 0.;
+    }
+
+    // Call the sum function
+    sum2(nelements, a, b, c);
+
+    // Check the sum
+    check_sum(nelements, c);
+
+    delete[] a;
+    delete[] b;
+    delete[] c;
+    exit(EXIT_SUCCESS);
+}
---
+++ C_sycl_case/func.cpp
@@ -0,0 +1,47 @@
+#include "func.hpp"
+#include <CL/sycl.hpp>
+
+using namespace sycl;
+
+void sum(int nele, double *inp1, double *inp2, double *res)
+{
+    for (int i = 0; i < nele; i++)
+        res[i] = inp1[i] + inp2[i];
+}
+
+void sum2(int nele, double *inp1, double *inp2, double *res)
+{
+
+    queue q;
+
+    // Create the buffers
+    size_t n = nele;
+    buffer<double, 1> a_buf(inp1, range<1>{n});
+    buffer<double, 1> b_buf(inp2, range<1>{n});
+    buffer<double, 1> c_buf(res, range<1>{n});
+
+    // Submit the command to the queue
+
+    q.submit([&](handler &h)
+             {
+                 auto a = a_buf.get_access<access::mode::read>(h);
+                 auto b = b_buf.get_access<access::mode::read>(h);
+                 auto c = c_buf.get_access<access::mode::write>(h);
+
+                 // Define the kernel
+                 h.parallel_for(range<1>{n}, [=](id<1> i)
+                                { c[i] = a[i] + b[i]; }); });
+}
+
+void check_sum(int n, double *res)
+{
+
+    for (int i = 0; i < n; i++)
+        if (fabs(res[i] - 3.0) > 1.E-15)
+        {
+            printf(" There is an error in the addition of vectors\n");
+            exit(EXIT_FAILURE);
+        }
+
+    printf(" No errors in the addition of vectors\n");
+}
